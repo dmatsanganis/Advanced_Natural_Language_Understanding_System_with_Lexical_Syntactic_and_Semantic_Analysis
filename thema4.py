32b74446b637fc3ca1a9926a9b6fb0d8c2c26f65
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-07-25 22:10:22 +0300

    Delete thema4.py

--- thema4.py
+++
@@ -1,8 +0,0 @@
-from nltk.tokenize import word_tokenize, sent_tokenize
-def sent_split(documents):
-    words = [word_tokenize(sent) for sent in sent_tokenize(text)]
-    return words
-
-text = 'Hello all. My name is Titipat, the best LoL player.'
-t=sent_split(text)
-print(t)
