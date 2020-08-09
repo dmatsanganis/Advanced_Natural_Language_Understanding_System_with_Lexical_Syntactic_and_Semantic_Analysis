26bdb9135923f33557526486a22d352847cf60d8
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-08-09 15:39:34 +0300

    Delete lexical_analyzer.py

--- Subject 4/lexical_analyzer.py
+++
@@ -1,8 +0,0 @@
-from nltk.tokenize import word_tokenize, sent_tokenize
-def sent_split(documents):
-    words = [word_tokenize(sent) for sent in sent_tokenize(text)]
-    return words
-
-text = 'My name is Nick and I am the best footballer in the world !'
-t=sent_split(text)
-print(t)
